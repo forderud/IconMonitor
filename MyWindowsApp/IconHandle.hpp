ebc27c309aa7d06b0db62148462f66d6eab2ea04
Fredrik Orderud <fredrik.orderud@ge.com>
2023-04-03 11:34:18 +0200

    Add diagnostics on second icon update call failing with res=65585 (0x10031) despite all icons being updated.

--- MyWindowsApp/IconHandle.hpp
+++ MyWindowsApp/IconHandle.hpp
@@ -15,11 +15,12 @@ public:
 
     void Activate(HWND wnd, bool as_big) {
         assert(m_icon);
-
+        LRESULT res = 0;
         if (as_big)
-            SendMessageW(wnd, WM_SETICON, ICON_BIG, (LPARAM)m_icon);   // update windws taskbar at bottom om screen
+            res = SendMessageW(wnd, WM_SETICON, ICON_BIG, (LPARAM)m_icon);   // update windws taskbar at bottom om screen
         else
-            SendMessageW(wnd, WM_SETICON, ICON_SMALL, (LPARAM)m_icon); // update window titlebar (top-left icon)
+            res = SendMessageW(wnd, WM_SETICON, ICON_SMALL, (LPARAM)m_icon); // update window titlebar (top-left icon)
+        // TODO: Figure out why second call fail with res=65585 (0x10031) despite all icons being updated
     }
 private:
     HICON m_icon = 0;
